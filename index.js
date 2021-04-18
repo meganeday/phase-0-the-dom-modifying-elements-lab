f7b2d3bb0413f516eea2fae8f3bf5b1babb3da2b
Megan Day <meganeday7@gmail.com>
2021-04-18 17:45:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove(main);
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+
+newHeader.innerHTML = "Megan is the champion";
