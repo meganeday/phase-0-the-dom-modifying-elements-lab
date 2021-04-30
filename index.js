ef2a0a51b96a67ebbd0dc2ece835a6543e106a30
Megan Day <meganeday7@gmail.com>
2021-04-29 19:48:14 -0500

    commit tree abe1d11b699f1d520895efae3632a1badb30eeaa

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // Write your code here!
-main.remove(main);
+// main.remove(main);
 
 const newHeader = document.createElement('h1');
 document.body.appendChild(newHeader);
 newHeader.id = 'victory';
 
 newHeader.innerHTML = "Megan is the champion";
+
+function removeMain() {
+    let mainElement = document.getElementById('main');
+    mainElement.remove(main)
+}
+
+removeMain();
